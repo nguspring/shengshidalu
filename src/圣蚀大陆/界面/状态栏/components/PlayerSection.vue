ebc1337ab2858055ea8b62a3d210da6efb4aafd9
nguspring <nguspring@users.noreply.github.com>
2026-07-10 23:30:26 +0800

    UI: 玩家区移除魔力同步率
    该字段在世界书中无独立设定，仅作为 MVU 规则存在，
    实际剧情中缺乏明确机制，状态栏显示价值低，直接删除。

--- src/圣蚀大陆/界面/状态栏/components/PlayerSection.vue
+++ src/圣蚀大陆/界面/状态栏/components/PlayerSection.vue
@@ -5,11 +5,6 @@
       <span class="msb-tag msb-tag-pro"><i class="fa-solid fa-user-astronaut"></i>{{ current_protagonist }}</span>
       <span class="msb-tag msb-tag-id"><i class="fa-solid fa-shield-halved"></i>{{ identity }}</span>
       <span class="msb-tag msb-tag-fac"><i class="fa-solid fa-scale-balanced"></i>{{ alignment }}</span>
-      <span class="msb-tag msb-tag-sync">
-        <i class="fa-solid fa-wand-magic-sparkles"></i>
-        <span class="msb-bar-wrap"><span class="msb-bar msb-bar-magic" :style="{ width: magic_sync + '%' }"></span></span>
-        {{ magic_sync }}%
-      </span>
     </div>
   </div>
 </template>
@@ -20,10 +15,9 @@ import { useDataStore } from '../store';
 
 const store = useDataStore();
 
-// 玩家区：当前扮演主角 / 身份 / 阵营偏向 / 魔力同步率（带进度条）
+// 玩家区：当前扮演主角 / 身份 / 阵营偏向
 // 主角从场景区移入，因为玩家操控的角色属于玩家身份
 const current_protagonist = computed(() => store.data.玩家.当前扮演主角);
 const identity = computed(() => store.data.玩家.身份);
 const alignment = computed(() => store.data.玩家.阵营偏向);
-const magic_sync = computed(() => store.data.玩家.魔力同步率);
 </script>
